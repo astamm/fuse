88855dbacf96a1f3ce7804a11a0a0b01bb2d881b
Aymeric Stamm <aymeric@stamm.fr>
2018-05-29 14:33:21 +0200

    Initial version of the package.

---
+++ R/FunctionalData.R
@@ -0,0 +1,334 @@
+#' \code{R6} Class for Functional Data
+#'
+#' @docType class
+#'
+#' @usage FunctionalData$new(grid = c(0, 2 * pi), value = sin, resolution =
+#'   1000L)
+#'
+#' @export
+#' @keywords data
+#' @return Object of \code{\link[R6]{R6Class}} with methods for communication
+#'   with lightning-viz server.
+#' @format An \code{\link[R6]{R6Class}} generator object.
+#'
+#' @examples
+#' f <- SobolevData$new(value = dnorm, grid = c(-5, 5))
+#' g <- BayesData$new(value = dnorm, grid = c(-5, 5))
+#' f
+#' g
+#' f$plot()
+#' g$plot()
+#' f$add(f)
+#' g$add(g)
+#' f$add(f)$plot()
+#' f$add()$plot()
+#' f$mul(2)$plot()
+#' g$add(g)$plot()
+#' g$mul(2)$plot()
+#' g$mul(1/2)$plot()
+#' g$div(2)$plot()
+#' f$mul(f)$plot()
+#' g$mul(g)$plot()
+#' f$prod(f$mul(2))
+#' f$dist(f$mul(2))
+#' f$dist(f$mul(2), normalize = TRUE)
+#'
+#' @field range Stores the grid range over which the functional datum is
+#'   defined.
+#'
+#' @section Methods: \describe{ \item{\code{new(grid = c(0, 2 * pi), value =
+#'   sin, resolution = 1000L)}}{This method is used to create object of this
+#'   class with \code{serveraddress} as address of the server object is
+#'   connecting to.} \item{\code{format(...)}}{This method changes server that
+#'   you are contacting with to \code{serveraddress}.}
+#'   \item{\code{evaluate(x)}}{This method creates new session on the server
+#'   with optionally given name in \code{sessionname}.}
+#'   \item{\code{plot()}}{This method changes currently used session on the
+#'   server to the one with id given in \code{sessionid} parameter.} }
+FunctionalData <- R6::R6Class(
+  classname = "FunctionalData",
+  cloneable = FALSE,
+  public = list(
+    initialize = function(grid = c(0, 2 * pi), value = sin, resolution = 1000L) {
+      # If value is a function, then grid can be either a range or a grid and the grid
+      # is then resampled to the input resolution.
+      # If value is a vector, then grid must be a vector of same size as
+      # value and this size defines the resolution, regardless of the input resolution.
+
+      stopifnot(is.numeric(grid) || "Date" %in% class(grid))
+
+      if (is.numeric(value)) {
+        resolution <- length(value)
+        stopifnot(length(grid) == resolution)
+        private$.resolution <- resolution
+        private$.range <- range(grid)
+        private$.value <- stats::splinefun(grid, value)
+      } else {
+        stopifnot(is.integer(resolution), length(resolution) == 1L)
+        stopifnot(length(grid) >= 2L)
+        private$.value <- rlang::as_function(value)
+        if (length(grid) == 2L) { # Consider it is giving the grid range
+          stopifnot(grid[1] < grid[2])
+          private$.resolution <- resolution
+          private$.range <- grid
+        } else {
+          private$.resolution <- min(resolution, length(grid))
+          private$.range <- range(grid)
+        }
+      }
+    },
+    format = function(...) {
+      c(
+        paste0("Functional Data:"),
+        paste0("  Grid size -------> ", private$.resolution, " points"),
+        paste0("  Validity range --> [", toString(round(private$.range, digits = 2L)), "]"),
+        paste0("  Available methods: "),
+        paste0("    ", names(self))
+      )
+    },
+    evaluate = function(x) {
+      private$.value(x)
+    },
+    copy = function() {
+      FunctionalData$new(
+        grid = private$.range,
+        value = private$.value,
+        resolution = private$.resolution
+      )
+    },
+    plot = function() {
+      df <- tibble::tibble(
+        t = seq(private$.range[1], private$.range[2], len = private$.resolution),
+        `f(t)` = private$.value(t)
+      )
+      ggplot2::ggplot(df, ggplot2::aes(t, `f(t)`)) +
+        ggplot2::geom_line() +
+        ggplot2::theme_bw()
+    },
+    add = function(x) {
+      invisible(self)
+    },
+    sub = function(x) {
+      invisible(self)
+    },
+    mul = function(x) {
+      invisible(self)
+    },
+    div = function(x) {
+      invisible(self)
+    },
+    pow = function(x) {
+      invisible(self)
+    },
+    apply = function(fun, ...) {
+      grid <- seq(private$.range[1], private$.range[2], len = private$.resolution)
+      values <- sapply(grid, private$.value)
+      self$initialize(grid, rlang::as_function(fun)(values, ...), private$.resolution)
+      invisible(self)
+    },
+    integrate = function(lower, upper, measure = "Lebesgue") {
+      lb <- private$.range[1]
+      if (missing(lower))
+        lower <- lb
+      else
+        stopifnot(lower >= lb)
+      ub <- private$.range[2]
+      if (missing(upper))
+        upper <- ub
+      else
+        stopifnot(upper <= ub)
+      switch (measure,
+        Lebesgue = integrate(self$evaluate, lower, upper)$value,
+        NA_real_
+      )
+    },
+    dist = function(x, normalize = FALSE) {
+      NA_real_
+    },
+    normdist = function(x) {
+      NA_real_
+    }
+  ),
+  active = list(
+    range = function(value) {
+      if (missing(value))
+        private$.range
+      else
+        stop("`$range` is read-only.", call. = FALSE)
+    },
+    resolution = function(value) {
+      if (missing(value))
+        private$.resolution
+      else
+        stop("`$resolution` is read-only.", call. = FALSE)
+    }
+  ),
+  private = list(
+    .resolution = integer(0L),
+    .range = numeric(0L),
+    .value = NULL,
+    .update = function(x) {
+      stopifnot("FunctionalData" %in% class(x) || is.numeric(x))
+      if ("FunctionalData" %in% class(x)) {
+        resolution <- min(private$.resolution, x$resolution)
+        lb <- max(private$.range[1], x$range[1])
+        ub <- min(private$.range[2], x$range[2])
+        range <- c(lb, ub)
+        grid <- seq(lb, ub, by = (ub - lb) / (resolution - 1))
+        x <- x$evaluate(grid)
+        y <- private$.value(grid)
+      } else {
+        lx <- length(x)
+        stopifnot(lx >= 1L)
+        if (lx == 1L) { # Scalar operation does not modify grid
+          resolution <- private$.resolution
+          x <- rep(x, resolution)
+        } else # Vector addition does modify grid
+          resolution <- min(private$.resolution, lx)
+        grid <- seq(private$.range[1], private$.range[2], len = resolution)
+        y <- private$.value(grid)
+      }
+      list(grid = grid, x = x, y = y, resolution = resolution)
+    }
+  )
+)
+
+#' @usage SobolevData$new(grid = c(0, 2 * pi), value = sin, resolution = 1000L)
+#'
+#' @export
+#' @rdname FunctionalData
+SobolevData <- R6::R6Class(
+  classname = "SobolevData",
+  cloneable = FALSE,
+  inherit = FunctionalData,
+  public = list(
+    format = function(...) {
+      c(
+        super$format(...),
+        paste0("  Space ----------> Sobolev")
+      )
+    },
+    copy = function() {
+      SobolevData$new(
+        grid = private$.range,
+        value = private$.value,
+        resolution = private$.resolution
+      )
+    },
+    add = function(x) {
+      if ("FunctionalData" %in% class(x))
+        stopifnot("SobolevData" %in% class(x))
+      x <- private$.update(x)
+      self$initialize(grid = x$grid, value = x$y + x$x, resolution = x$resolution)
+      invisible(self)
+    },
+    sub = function(x) {
+      if ("FunctionalData" %in% class(x))
+        stopifnot("SobolevData" %in% class(x))
+      x <- private$.update(x)
+      self$initialize(grid = x$grid, value = x$y - x$x, resolution = x$resolution)
+      invisible(self)
+    },
+    mul = function(x) {
+      if ("FunctionalData" %in% class(x))
+        stopifnot("SobolevData" %in% class(x))
+      x <- private$.update(x)
+      self$initialize(grid = x$grid, value = x$y * x$x, resolution = x$resolution)
+      invisible(self)
+    },
+    div = function(x) {
+      if ("FunctionalData" %in% class(x))
+        stopifnot("SobolevData" %in% class(x))
+      x <- private$.update(x)
+      self$initialize(grid = x$grid, value = x$y / x$x, resolution = x$resolution)
+      invisible(self)
+    },
+    pow = function(x) {
+      if ("FunctionalData" %in% class(x))
+        stopifnot("SobolevData" %in% class(x))
+      x <- private$.update(x)
+      self$initialize(grid = x$grid, value = x$y ^ x$x, resolution = x$resolution)
+      invisible(self)
+    },
+    inner = function(x) {
+      if ("FunctionalData" %in% class(x))
+        stopifnot("SobolevData" %in% class(x))
+      self$mul(x)$integrate()
+    },
+    norm = function() {
+      sqrt(self$inner(self))
+    },
+    normalize = function() {
+      self$div(self$norm())
+    },
+    dist = function(x, normalize = FALSE) {
+      if ("FunctionalData" %in% class(x))
+        stopifnot("SobolevData" %in% class(x))
+      out <-
+      if (!normalize) return(self$sub(x)$pow(2L)$integrate())
+      p <- self$sub(x)$pow(2L)
+      p$integrate() / as.numeric(diff(p$range))
+    },
+    normdist = function(x) {
+      if ("FunctionalData" %in% class(x))
+        stopifnot("SobolevData" %in% class(x))
+      x <- private$.update(x)
+      e1 <- SobolevData$new(grid = x$grid, value = x$y, resolution = x$resolution)
+      e2 <- SobolevData$new(grid = x$grid, value = x$x, resolution = x$resolution)
+      e1$norm() - e2$norm()
+    }
+  )
+)
+
+#' @usage BayesData$new(grid = c(0, 2 * pi), value = sin, resolution = 1000L)
+#'
+#' @export
+#' @rdname FunctionalData
+BayesData <- R6::R6Class(
+  classname = "BayesData",
+  cloneable = FALSE,
+  inherit = FunctionalData,
+  public = list(
+    format = function(...) {
+      c(
+        super$format(...),
+        paste0("  Space ----------> Bayes")
+      )
+    },
+    copy = function() {
+      BayesData$new(
+        grid = private$.range,
+        value = private$.value,
+        resolution = private$.resolution
+      )
+    },
+    add = function(x) {
+      if ("FunctionalData" %in% class(x))
+        stopifnot(" BayesData" %in% class(x))
+      x <- private$.update(x)
+      self$initialize(grid = x$grid, value = x$y * x$x, resolution = x$resolution)
+      invisible(self)
+    },
+    sub = function(x) {
+      if ("FunctionalData" %in% class(x))
+        stopifnot(" BayesData" %in% class(x))
+      x <- private$.update(x)
+      self$initialize(grid = x$grid, value = x$y / x$x, resolution = x$resolution)
+      invisible(self)
+    },
+    mul = function(x) {
+      if ("FunctionalData" %in% class(x))
+        stopifnot(" BayesData" %in% class(x))
+      x <- private$.update(x)
+      self$initialize(grid = x$grid, value = x$y ^ x$x, resolution = x$resolution)
+      invisible(self)
+    },
+    div = function(x) {
+      if ("FunctionalData" %in% class(x))
+        stopifnot(" BayesData" %in% class(x))
+      x <- private$.update(x)
+      self$initialize(grid = x$grid, value = x$y ^ (1 / x$x), resolution = x$resolution)
+      invisible(self)
+    }
+  )
+)
---
+++ R/Operations.R
@@ -0,0 +1,60 @@
+#' \code{FunctionalData} Group Generic Functions
+#'
+#' These functions provide methods for applying
+#' \code{\link[base]{groupGeneric}}s to \code{\link{FunctionalData}} objects.
+#' Note that unary operators in \code{Ops} group (\code{"&"}, \code{"|"} and
+#' \code{"!"}) are not meaningful for \code{\link{FunctionalData}} objects and
+#' thus not implemented.
+#'
+#' @inherit base::groupGeneric
+#'
+#' @examples
+#' f <- SobolevData$new(c(0, 2 * pi), ~ .)
+#' g <- sin(f)
+#' z1 <- f + g
+#' y0 <- 1:10
+#' z2 <- f + y0
+#' @name groupGeneric.FunctionalData
+NULL
+
+#' @export
+#' @rdname groupGeneric.FunctionalData
+Math.FunctionalData <- function(x, ...) {
+  x$copy()$apply(.Generic, ...)
+}
+
+#' @export
+#' @rdname groupGeneric.FunctionalData
+Ops.FunctionalData <- function(e1, e2) {
+  if (missing(e2)) {
+    # code for unary operations
+    stop("Unary operators &, | and ! are not meaningful for FunctionalData objects.")
+  } else {
+    # code for binary operations
+    fd1 <- "FunctionalData" %in% class(e1)
+    fd2 <- "FunctionalData" %in% class(e2)
+    if (!fd1 && fd2) {
+      v <- e1
+      e1 <- e2
+      e2 <- v
+    }
+    if ("FunctionalData" %in% class(e1)) {
+      switch(
+        .Generic,
+        "+" = e1$copy()$add(e2),
+        "-" = e1$copy()$sub(e2),
+        "*" = e1$copy()$mul(e2),
+        "/" = e1$copy()$div(e2),
+        "^" = e1$copy()$pow(e2),
+        "%/%" = e1$copy()$div(e2)$apply(round),
+        "%%" = e1$copy()$sub(e1$copy()$div(e2)$apply(round)$mul(e2)),
+        "==" = e1$copy()$dist(e2) == 0,
+        "!=" = e1$copy()$dist(e2) != 0,
+        "<" = e1$normdist(e2) < 0,
+        "<=" = e1$normdist(e2) <= 0,
+        ">" = e1$normdist(e2) > 0,
+        ">=" = e1$normdist(e2) >= 0
+      )
+    }
+  }
+}
